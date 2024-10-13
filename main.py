43e9ade317efa3a068023c02fcca2877c0ec8a89
Ярослав <Yagavdv14@yandex.ru>
2024-10-13 11:00:24 +0300

    имитация нажатия клавиш, в коде строки 34-44

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from yandexbrowser import YaBrowser
 from msedgebrowser import MsEdge
 from firefoxbrowser import FireFox
 from time import sleep
-
+from selenium.webdriver.common.keys import Keys
 url_base = 'https://saucedemo.com'
 url_catalog = 'https://www.saucedemo.com/inventory.html'
 element_catalog = 'Products'
@@ -12,7 +12,7 @@ password_tuple = ('secret_sauce', 'wrong_password', '')
 
 # родитель: класс ChromeBrowser, дочки: YaBrowser(ручное обновление), MsEdge, FireFox
 # для проверки оставь в кортеже те который есть у тебя (для этого импортировал все классы)
-browser_tuple = (YaBrowser,MsEdge, FireFox)
+browser_tuple = (YaBrowser, MsEdge, FireFox)
 
 # Сначала проверка для действительного пароля для всех юзеров (и пустого)
 # Затем для некорректного пароля и пустого
@@ -26,15 +26,28 @@ for password in password_tuple:
                 # Загрузка сайта
                 ibrowser.get_url(url_base)
 
-                # Авторизация на сайте
-                ibrowser.send_keys_by_xpath("//input[@id='user-name']", user)
-                # обновление поставлено здесь так как ошибочный ввод логина или пароля поднимает исключение
-                sleep(3)
-                ibrowser.driver.refresh()
-                sleep(3)
+                # ВВод логина и пароля для дальнейшей работы
                 ibrowser.send_keys_by_xpath("//input[@id='user-name']", user)
                 ibrowser.send_keys_by_xpath("//input[@id='password']", password)
-                ibrowser.click_by_xpath("//input[@id='login-button']")
+                sleep(2)
+
+                # Имитация нажатия клавиш
+                ibrowser.send_keys_by_xpath("//input[@id='user-name']", Keys.CONTROL + 'a')
+                sleep(2)
+                ibrowser.send_keys_by_xpath("//input[@id='user-name']", Keys.BACKSPACE)
+                sleep(1)
+
+                # Вариант без импорта Keys
+                ibrowser.send_keys_by_xpath("//input[@id='password']", '\ue009 a')
+                sleep(2)
+                ibrowser.send_keys_by_xpath("//input[@id='password']", '\ue003')
+                sleep(3)
+
+                # Авторизация на сайте
+                ibrowser.send_keys_by_xpath("//input[@id='user-name']", user)
+                ibrowser.send_keys_by_xpath("//input[@id='password']", password)
+                ibrowser.send_keys_by_xpath("//input[@id='password']", Keys.ENTER)
+                # ibrowser.click_by_xpath("//input[@id='login-button']")
                 # проверка на наличия контейнера с ошибкой и вывод ее текста
                 ibrowser.check_element_and_his_null_text("//div[@class='error-message-container error']")
                 # не стал добавлять клик по крестику ibrowser.click_by_xpath("//button[@class='error-button']")
