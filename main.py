5527ff8f3808c30b99d6cea12ce60b91c802b62c
Ярослав <Yagavdv14@yandex.ru>
2024-10-19 12:23:33 +0300

    Исправил,

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from yandexbrowser import YaBrowser
 from msedgebrowser import MsEdge
 from firefoxbrowser import FireFox
 from time import sleep
-from selenium.webdriver.common.keys import Keys
 
 url_base = 'https://saucedemo.com'
 url_catalog = 'https://www.saucedemo.com/inventory.html'
@@ -30,7 +29,7 @@ for password in password_tuple:
                 # Авторизация на сайте
                 ibrowser.send_keys_by_xpath("//input[@id='user-name']", user)
                 ibrowser.send_keys_by_xpath("//input[@id='password']", password)
-                ibrowser.send_keys_by_xpath("//input[@id='password']", Keys.ENTER)
+                ibrowser.send_keys_by_xpath("//input[@id='password']", "\ue007")
 
                 # проверка на наличия контейнера с ошибкой и вывод ее текста
                 ibrowser.check_element_and_his_null_text("//div[@class='error-message-container error']")
@@ -40,13 +39,30 @@ for password in password_tuple:
                 ibrowser.check_current_url(url_catalog)
                 ibrowser.check_by_element_on_page(element_catalog, "//span[@class='title']")
 
-                # добавление товаров в корзину, переход в нее и пролистывание
-                ibrowser.add_all_products_to_cart()
+                # добавление товаров в корзину
+                products = ibrowser.get_elements_as_list_by_xpath("//div[@class='inventory_item_name ']")
+                buttons = (ibrowser.get_elements_as_list_by_xpath(
+                    "//button[@class='btn btn_primary btn_small btn_inventory ']"))
+                assert len(products) == len(buttons), 'Количество товаров и кнопок добавить товар не совпадает'
+
+                print('Добавление товаров в корзину:\n')
+                for counter in range(len(products)):
+                    buttons[counter].click()
+                    print(f'\tТовар "{products[counter].text}" добавлен в корзину')
+                print(f'\n\tВсего добавлено в корзину наименований: {len(products)}')
+
+                # переход в корзину
                 ibrowser.click_by_xpath("//a[@class='shopping_cart_link']")
-                ibrowser.scroll_to_last_element_in_cart()
+
+                # пролистывание до последней позиции
+                products_in_cart = ibrowser.get_elements_as_list_by_xpath("//div[@class='cart_item']")
+                ibrowser.scroll_to_element(products_in_cart[-1])
 
             except AssertionError as err:
                 print('❌Возникла ошибка!\n\t', err.args[0], '\n')
+
+            except Exception as err:
+                print(err)
 
             else:
                 # Информационное сообщение в консоль о завершении
