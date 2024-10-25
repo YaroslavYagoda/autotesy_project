771f7411d90849ca025e25d17ac5e803712c1751
Ярослав <Yagavdv14@yandex.ru>
2024-10-25 11:48:59 +0300

    Готово

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ password_tuple = ('secret_sauce',)
 
 # родитель: класс ChromeBrowser, дочки: YaBrowser(ручное обновление), MsEdge, FireFox
 # для проверки оставь в кортеже те который есть у тебя (для этого импортировал все классы)
-browser_tuple = (MsEdge,)
+browser_tuple = (YaBrowser,)
 
 for password in password_tuple:
     for user in user_tuple:
@@ -26,47 +26,16 @@ for password in password_tuple:
                 # Загрузка сайта
                 ibrowser.get_url(url_base)
 
-                # ВВодит и Выделяет значения полей логин и пароль и удаляет значения полей
-                ibrowser.send_keys_by_xpath("//input[@id='user-name']", "какой-то логин")
-                ibrowser.send_keys_by_xpath("//input[@id='password']", "и пароль")
-                ibrowser.send_keys_by_xpath("//input[@id='user-name']", Keys.CONTROL + "a")
-                ibrowser.send_keys_by_xpath("//input[@id='user-name']", Keys.DELETE)
-                ibrowser.send_keys_by_xpath("//input[@id='password']", Keys.CONTROL + "a")
-                ibrowser.send_keys_by_xpath("//input[@id='password']", Keys.BACKSPACE)
-
                 # Авторизация на сайте
                 ibrowser.send_keys_by_xpath("//input[@id='user-name']", user)
                 ibrowser.send_keys_by_xpath("//input[@id='password']", password)
                 ibrowser.send_keys_by_xpath("//input[@id='password']", Keys.ENTER)
 
-                """
-                # проверка на наличия контейнера с ошибкой и вывод ее текста
-                ibrowser.check_element_and_his_null_text("//div[@class='error-message-container error']")
-                print('Авторизация на сайте проведена')
+                # Выход с сайта
+                ibrowser.click_by_xpath("//button[@id='react-burger-menu-btn']")
+                sleep(1)
+                ibrowser.click_by_xpath("//a[@id='logout_sidebar_link']")
 
-                # Проведение тестов
-                ibrowser.check_current_url(url_catalog)
-                ibrowser.check_by_element_on_page(element_catalog, "//span[@class='title']")
-
-                # добавление товаров в корзину
-                products = ibrowser.get_elements_as_list_by_xpath("//div[@class='inventory_item_name ']")
-                buttons = (ibrowser.get_elements_as_list_by_xpath(
-                    "//button[@class='btn btn_primary btn_small btn_inventory ']"))
-                assert len(products) == len(buttons), 'Количество товаров и кнопок добавить товар не совпадает'
-
-                print('Добавление товаров в корзину:\n')
-                for counter in range(len(products)):
-                    buttons[counter].click()
-                    print(f'\tТовар "{products[counter].text}" добавлен в корзину')
-                print(f'\n\tВсего добавлено в корзину наименований: {len(products)}')
-
-                # переход в корзину
-                ibrowser.click_by_xpath("//a[@class='shopping_cart_link']")
-
-                # пролистывание до последней позиции
-                products_in_cart = ibrowser.get_elements_as_list_by_xpath("//div[@class='cart_item']")
-                ibrowser.scroll_to_element(products_in_cart[-1])
-                """
             except AssertionError as err:
                 print('❌Возникла ошибка!\n\t', err.args[0], '\n')
 
