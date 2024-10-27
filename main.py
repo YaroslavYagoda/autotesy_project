f61e5cf65b31549b5eecec98db50bad0f6af02e0
Ярослав <Yagavdv14@yandex.ru>
2024-10-27 14:56:01 +0300

    Назад/Вперед

--- main.py
+++ main.py
@@ -25,59 +25,14 @@ for browser in browser_tuple:
     ibrowser.send_keys_by_xpath("//input[@id='password']", password)
     ibrowser.send_keys_by_xpath("//input[@id='password']", Keys.ENTER)
 
-    # добавление товаров в корзину
-    products = ibrowser.driver.find_elements(By.XPATH, "//div[@class='inventory_item']")
-    buttons = ibrowser.driver.find_elements(By.XPATH, "//button[@class='btn btn_primary btn_small btn_inventory ']")
-    products_info = []
-    print('Добавление товаров в корзину:')
-    for counter in range(2, 4):
-        price = products[counter].find_element(By.XPATH, ".//div[@class='inventory_item_price']").text
-        name = products[counter].find_element(By.XPATH, ".//div[@class='inventory_item_name ']").text
-        products_info.append([name, price])
-        buttons[counter].click()
-        print(f'\tТовар "{name}" добавлен в корзину')
-    print(f'Добавлено в корзину товаров: {len(products_info)}')
-
     # переход в корзину
     ibrowser.click_by_xpath("//a[@class='shopping_cart_link']")
-    products_in_cart = ibrowser.driver.find_elements(By.XPATH, "//div[@class='cart_item']")
-    for counter in range(len(products_info)):
-        assert (products_info[counter][0] ==
-                products_in_cart[counter].find_element(By.XPATH, ".//div[@class='inventory_item_name']").text), \
-            f'Наименование товара в корзине под номером {counter + 1} не верное'
-        assert (products_info[counter][1] ==
-                products_in_cart[counter].find_element(By.XPATH, ".//div[@class='inventory_item_price']").text), \
-            f'Цена товара в корзине под номером {counter + 1} не верная'
-    print('Товар в корзине проверен')
 
-    # Оформление покупки (ввод данных покупателя)
-    ibrowser.click_by_xpath("//button[@class='btn btn_action btn_medium checkout_button ']")
-    ibrowser.send_keys_by_xpath("//input[@id='first-name']", "111")
-    ibrowser.send_keys_by_xpath("//input[@id='last-name']", "111")
-    ibrowser.send_keys_by_xpath("//input[@id='postal-code']", "5658")
-    ibrowser.click_by_xpath("//input[@id='continue']")
-    print('Сведения о покупателе внесены')
-
-    # Финальная проверка товара
-    products_in_cart = ibrowser.driver.find_elements(By.XPATH, "//div[@class='cart_item']")
-    for counter in range(len(products_info)):
-        assert (products_info[counter][0] ==
-                products_in_cart[counter].find_element(By.XPATH, ".//div[@class='inventory_item_name']").text), \
-            f'Наименование товара под номером {counter + 1} на странице оформления товара не верное'
-        assert (products_info[counter][1] ==
-                products_in_cart[counter].find_element(By.XPATH, ".//div[@class='inventory_item_price']").text), \
-            f'Цена товара под номером {counter + 1} на странице оформления товара не верная'
-    print('Товар на странице оформления товара проверен')
-
-    # Проверка итоговой цены
-    total_price = ibrowser.driver.find_element(By.XPATH, "//div[@class='summary_subtotal_label']").text
-    summary_price_products = f'Item total: ${sum([float(i[1][1:]) for i in products_info])}'
-    assert summary_price_products == total_price, 'Итоговая сумма всех товаров неверная'
-    print('Итоговая сумма товаров (без налога) проверена')
-
-    # Проверка успешного оформления заказа
-    ibrowser.click_by_xpath("//button[@id='finish']")
-    ibrowser.check_by_element_on_page("Thank you for your order!", "//h2[@class='complete-header']")
+    # Домашнее задание
+    sleep(1)
+    ibrowser.driver.back()
+    sleep(1)
+    ibrowser.driver.forward()
 
     # Завершение работы браузера
     ibrowser.quit()
