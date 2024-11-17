09d1f817d4f6d8529171a641769363d1b5cf17f3
Ярослав <Yagavdv14@yandex.ru>
2024-11-17 15:13:17 +0300

    Переключатели

--- main.py
+++ main.py
@@ -7,9 +7,8 @@ from msedgebrowser import MsEdge
 from firefoxbrowser import FireFox
 from time import sleep
 
-url_base = 'https://demoqa.com/checkbox'
-#user = "standard_user"
-#password = "secret_sauce"
+url_base = 'https://demoqa.com/radio-button'
+
 # родитель: класс ChromeBrowser, дочки: YaBrowser(ручное обновление), MsEdge, FireFox
 # для проверки оставь в кортеже те который есть у тебя (для этого импортировал все классы)
 browser_tuple = (YaBrowser,)
@@ -21,25 +20,20 @@ for browser in browser_tuple:
     # Загрузка сайта
     ibrowser.get_url(url_base)
 
-    # Выбор и проверка чекбокса
-    check_box = ibrowser.driver.find_element(By.XPATH, '//span[@class="rct-checkbox"]')
-    check_box.click()
-    if check_box.is_selected():
-        print('Чек-бокс выбран')
-    else:
-        print('Чек-бокс не выбран')
-        sleep(2)
-    """
-    На самом деле локатор '//span[@class="rct-checkbox"]' не даст выбрать какой либо другой чек-бокс
-    кроме как первый, выбор локатора в уроке выбран странно я бы сделал по другому (ниже код)
-    """
-    check_box = ibrowser.driver.find_element(By.XPATH, '//label[@for="tree-node-home"]/span[1]')
-    check_box.click()
-    if check_box.is_selected():
-        print('Чек-бокс выбран')
-    else:
-        print('Чек-бокс не выбран')
+    # Домашнее задание
+    radio_button_1 = ibrowser.driver.find_element(By.XPATH, '//label[@for="yesRadio"]')
+    radio_button_2 = ibrowser.driver.find_element(By.XPATH, '//label[@for="impressiveRadio"]')
+
+    radio_button_1.click()
+    print(f'Переключатель один статус нажатия: {radio_button_1.find_element(By.XPATH, '..//input[1]').is_selected()}')
+    print(f'Переключатель два статус нажатия: {radio_button_2.find_element(By.XPATH, '..//input[1]').is_selected()}')
     sleep(2)
+
+    radio_button_2.click()
+    print(f'Переключатель один статус нажатия: {radio_button_1.find_element(By.XPATH, '..//input[1]').is_selected()}')
+    print(f'Переключатель два статус нажатия: {radio_button_2.find_element(By.XPATH, '..//input[1]').is_selected()}')
+    sleep(2)
+
     # Завершение работы браузера
     ibrowser.quit()
     print(f'Процесс браузера {browser.__name__} завершен\n\n')
