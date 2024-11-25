202779a376888a944772646d990d82b107ded099
Ярослав <Yagavdv14@yandex.ru>
2024-11-25 23:28:54 +0300

    work with dropdown

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import datetime
 from selenium.webdriver import Keys, ActionChains
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
 from chromebrowser import ChromeBrowser
 from yandexbrowser import YaBrowser
 from msedgebrowser import MsEdge
 from firefoxbrowser import FireFox
 from time import sleep
 
-url_base = 'https://html5css.ru/howto/howto_js_rangeslider.php'
+url_base = 'https://www.lambdatest.com/selenium-playground/jquery-dropdown-search-demo'
 
 # родитель: класс ChromeBrowser, дочки: YaBrowser(ручное обновление), MsEdge, FireFox
 # для проверки оставь в кортеже те который есть у тебя (для этого импортировал все классы)
@@ -20,42 +21,12 @@ for browser in browser_tuple:
     # Загрузка сайта
     ibrowser.get_url(url_base)
     sleep(0.5)
+
     # Домашнее задание
-    action = ActionChains(ibrowser.driver)
-    slider = ibrowser.driver.find_element(By.XPATH, '//input[@class="slider-color"]')
-
-    # Получение атрибутов значения слайдера для определения шага в пиксилях для одной единицы
-    current_value = int(slider.get_attribute('value'))
-
-    # Поиск шага в пикселях для одной единицы и поправки для увеличения
-    slider_value = ibrowser.driver.find_element(By.XPATH, '//span[@id="f"]')
-    pixel_step = 0
-    koff = 0
-
-    # поправка
-    while int(slider_value.text) == current_value:
-        koff += 1
-        action.click_and_hold(slider).move_by_offset(koff, 0).release().perform()
-
-    # возврат на исходную позицию
-    action.click_and_hold(slider).move_by_offset(0, 0).release().perform()
-
-    # шаг
-    while int(slider_value.text) == current_value:
-        pixel_step += 1
-        action.click_and_hold(slider).move_by_offset(-pixel_step, 0).release().perform()
-
-    print(f'Шаг для 1 ед. = {pixel_step},поправка - {koff}')
-
-    # Проверка правильности шага и поправки для значения 38 и 61
-    action.click_and_hold(slider).move_by_offset(pixel_step * (-12), 0).release().perform()
-    assert int(slider_value.text) == 38, f'Значение ползунка - {slider_value.text} должно быть 38!'
-
-    action.click_and_hold(slider).move_by_offset(pixel_step * 11 - koff, 0).release().perform()
-    assert int(slider_value.text) == 61, f'Значение ползунка - {slider_value.text} должно быть 61!'
-
-    print('Шаг проверен')
-    sleep(1)
+    ibrowser.click_by_xpath('//span[@class="select2-selection select2-selection--single"][1]')
+    # Выбор Japan
+    ibrowser.click_by_xpath('//li[@class="select2-results__option"][6]')
+    sleep(3)
 
     # Завершение работы браузера
     ibrowser.quit()
