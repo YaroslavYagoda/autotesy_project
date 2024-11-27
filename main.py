1c7113b3dcf3830ba624336359a920d593ee2b52
Ярослав <Yagavdv14@yandex.ru>
2024-11-27 12:35:26 +0300

    Ввод фейкового имени

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import datetime
+from faker import Faker
 from selenium.webdriver import Keys, ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
@@ -8,7 +9,7 @@ from msedgebrowser import MsEdge
 from firefoxbrowser import FireFox
 from time import sleep
 
-url_base = 'https://www.lambdatest.com/selenium-playground/iframe-demo/'
+url_base = 'https://www.saucedemo.com/'
 
 # родитель: класс ChromeBrowser, дочки: YaBrowser(ручное обновление), MsEdge, FireFox
 # для проверки оставь в кортеже те который есть у тебя (для этого импортировал все классы)
@@ -23,32 +24,14 @@ for browser in browser_tuple:
     sleep(1)
 
     # Домашнее задание
-    # Переключение на вложенный контейнер
-    iframe = ibrowser.driver.find_element(By.XPATH, '//iframe[@id="iFrame1"]')
-    ibrowser.driver.switch_to.frame(iframe)
-
     # Подготовка исходных данных
-    text_box_locator = '//div[@class="rsw-ce"]'
-    text_input = 'Test message'
-    button_bold_locator = '//button[@title="Bold"]'
-    button_italic_locator = '//button[@title="Italic"]'
-
-    # Вdод текста в поле и проверка корректности ввода
-    ibrowser.send_keys_by_xpath(text_box_locator, Keys.CONTROL+"A")
-    ibrowser.send_keys_by_xpath(text_box_locator, Keys.DELETE)
-    ibrowser.send_keys_by_xpath(text_box_locator, text_input)
-    text_result = ibrowser.value_by_xpath(text_box_locator)
-    assert text_input == text_result, f'Ожидается:{text_input}, получено: {text_result} '
-    print('Текст введен корректно')
-    sleep(0.2)
-
-    # Редактирование текста и проверка его корректности после изменений
-    ibrowser.send_keys_by_xpath(text_box_locator, Keys.CONTROL + "A")
-    ibrowser.click_by_xpath(button_bold_locator)
-    ibrowser.click_by_xpath(button_italic_locator)
-    text_result = ibrowser.value_by_xpath(text_box_locator)
-    assert text_input == text_result, f'Ожидается:{text_input}, получено: {text_result} '
-    print('Текст после изменения внешнего вида корректен')
+    user_name_locator = '//input[@id="user-name"]'
+    fake = Faker('en_US')
+    fake_name = fake.first_name()
+    print(f'Создано фейковое имя: {fake_name}')
+    # Ввод данных фейковых данных
+    ibrowser.send_keys_by_xpath(user_name_locator, fake_name)
+    print('Фейковое имя введенов строку логина')
     sleep(2)
 
     # Завершение работы браузера
