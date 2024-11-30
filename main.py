face1fac9307bb3307a2c63a6ecf7bfa033ef33c
Ярослав <Yagavdv14@yandex.ru>
2024-11-30 08:17:21 +0300

    Обработка исключений

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
-import datetime
-
-from faker import Faker
 from selenium.webdriver import Keys, ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
+from selenium.common import exceptions as Sel_exsept
+
 from chromebrowser import ChromeBrowser
 from yandexbrowser import YaBrowser
 from msedgebrowser import MsEdge
 from firefoxbrowser import FireFox
-from time import sleep
-import os
 
-url_base = 'https://www.lambdatest.com/selenium-playground/download-file-demo'
-path_for_download = os.path.dirname(os.path.abspath(__file__)) + '\\downloads'
-file_name = path_for_download + '\\' + 'LambdaTest.pdf'
-file_size = 100
+from time import sleep
+
+url_base = 'https://demoqa.com/dynamic-properties'
+
 # родитель: класс ChromeBrowser, дочки: YaBrowser(ручное обновление), MsEdge, FireFox
 # для проверки оставь в кортеже те который есть у тебя (для этого импортировал все классы)
 browser_tuple = (YaBrowser,)
@@ -25,28 +22,25 @@ for browser in browser_tuple:
 
     # Загрузка сайта
     ibrowser.get_url(url_base)
-    sleep(1)
+    sleep(3)
 
-    # Загрузка файла
-    ibrowser.click_by_xpath('//button[@class="btn btn-primary"]')
-
-    # Проверка загрузки файла
-    wait_seс = 0
-    while not os.access(file_name, os.F_OK) and wait_seс < 10:
-        sleep(1)
-        wait_seс += 1
-
-    assert os.access(file_name, os.F_OK), \
-        'Ошибка загрузки файла'
-    print('Файл загружен')
-
-    assert os.path.getsize(file_name) > file_size, \
-        'Размер файла не соответствует контрольному размеру'
-    print('Файл, предположительно, загружен полностью')
-
-    # Удаление загруженного файла
-    os.remove(file_name)
-    print('Файл удален\n')
+    # Домашняя работа
+    wait_sec = 0
+    max_sec = 2
+    check = False
+    while not check and wait_sec < max_sec:
+        try:
+            ibrowser.click_by_xpath('//button[@id="visibleAfter"]')
+        except Sel_exsept.NoSuchElementException as err:
+            print(f'Попытка № {wait_sec+1}. Возникла ошибка {err.__class__.__name__}.')
+        else:
+            check = True
+            print(f'Попытка № {wait_sec+1}. Кнопка нажата')
+        finally:
+            wait_sec += 1
+            sleep(1)
+    if wait_sec == max_sec:
+        print('Элемент не был обнаружен. Лимит попыток исчерпан')
 
     # Завершение работы браузера
     ibrowser.quit()
